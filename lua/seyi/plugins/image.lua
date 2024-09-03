ca02e47a5788e224cc639aa0ffef72b2c13a36e8
Odukoya Jesuseyitan <odukoyajesuseyitan@gmail.com>
2024-09-03 23:27:31 +0100

    Add image support to neovim
    Co-authored-by: Seyi <setoryz@gmail.com>

---
+++ lua/seyi/plugins/image.lua
@@ -0,0 +1,43 @@
+return {
+  "3rd/image.nvim",
+  -- Disable on windows system
+  cond = function()
+    return vim.fn.has("win32") ~= 1
+  end,
+  dependencies = {
+    "leafo/magick",
+  },
+  opts = {},
+  config = function()
+    local image = require("image")
+
+    image.setup({
+      backend = "kitty", -- use kitty backend that is also supported in wezterm
+      integrations = {
+        markdown = {
+          enabled = true,
+          clear_in_insert_mode = false,
+          download_remote_images = true,
+          only_render_image_at_cursor = false,
+          filetypes = { "markdown", "vimwiki" }, -- Markdown extensions
+        },
+        html = {
+          enabled = false,
+        },
+        css = {
+          enabled = false,
+        },
+      },
+      max_width = nil,
+      max_height = nil,
+      max_width_window_percentage = nil,
+      max_height_window_percentage = nil,
+      window_overlap_clear_enabled = false, -- toggles images when windows are overlapped
+      window_overlap_clear_ft_ignore = { "cmp_menu", "cmp_docs", "" },
+      editor_only_render_when_focused = false, -- auto show/hide images whe the editor gains/looses focus
+      tmux_show_only_in_active_window = false, -- auto show/hide images in the correct tmux window
+      -- TODO: Figure out ico files
+      hijack_file_patterns = { "*.png", "*.ico", "*.jpg", "*.jpeg", "*.gif", "*.webp", "*.avif" }, -- render image files as images when opened
+    })
+  end,
+}
