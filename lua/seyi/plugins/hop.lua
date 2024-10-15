9acfaeb51b159204f2ed5dfce537b49de4af161c
Odukoya Jesuseyitan <odukoyajesuseyitan@gmail.com>
2024-10-15 15:43:39 +0100

    add hop for better vim navigation
    Co-authored-by: Seyi <setoryz@gmail.com>

---
+++ lua/seyi/plugins/hop.lua
@@ -0,0 +1,17 @@
+return {
+  "smoka7/hop.nvim",
+  opts = {
+    multi_windows = true,
+    keys = "etovxqpdygfblzhckisuran",
+    uppercase_labels = true,
+  },
+  keys = {
+    {
+      "<leader>hop",
+      function()
+        require("hop").hint_words()
+      end,
+      mode = { "n", "x", "o" },
+    },
+  },
+}
