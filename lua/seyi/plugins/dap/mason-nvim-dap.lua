ca5bb9ebcc0f3dae0692f931e722c7092a391536
Odukoya Jesuseyitan <odukoyajesuseyitan@gmail.com>
2025-03-14 23:46:22 +0100

    Extend debugger for go

--- lua/seyi/plugins/dap/mason-nvim-dap.lua
+++ lua/seyi/plugins/dap/mason-nvim-dap.lua
@@ -4,6 +4,7 @@ return {
     "williamboman/mason.nvim",
     "mfussenegger/nvim-dap",
   },
+  cmd = { "DapInstall", "DapUninstall" },
   config = function()
     local mason_nvim_dap = require("mason-nvim-dap")
 
@@ -11,7 +12,10 @@ return {
     mason_nvim_dap.setup({
       -- List of adapters for mason to install
 
-      ensure_installed = { "js" },
+      ensure_installed = {
+        "delve",
+        "js",
+      },
       automatic_installation = true,
     })
   end,
