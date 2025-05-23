9300f880ce30ad60f8907375d45f5d8f3bb60a6d
Odukoya Jesuseyitan <odukoyajesuseyitan@gmail.com>
2025-05-23 13:06:30 +0100

    chore: update keymap to not toggle file explorer when opening current file

--- lua/seyi/plugins/nvim-tree.lua
+++ lua/seyi/plugins/nvim-tree.lua
@@ -56,7 +56,7 @@ return {
     local keymap = vim.keymap
 
     keymap.set("n", "<leader>ee", "<cmd>NvimTreeToggle<CR>", { desc = "Toggle file explorer" }) -- toggle file explorer
-    keymap.set("n", "<leader>ef", "<cmd>NvimTreeFindFileToggle<CR>", { desc = "Toggle file explorer on current file" }) -- toggle file explorer on current opened file
+    keymap.set("n", "<leader>ef", "<cmd>NvimTreeFindFile<CR>", { desc = "Toggle file explorer on current file" }) -- toggle file explorer on current opened file
     keymap.set("n", "<leader>ec", "<cmd>NvimTreeCollapse<CR>", { desc = "Collapse file Explorer" }) -- collapse file explorer
     keymap.set("n", "<leader>er", "<cmd>NvimTreeRefresh<CR>", { desc = "Refresh file Explorer" }) -- refresh file explorer
   end,
